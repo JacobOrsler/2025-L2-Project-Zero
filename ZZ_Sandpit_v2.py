c00baae4d16f02eb844b341196a678b9e6b2d6e3
JacobOrsler <jacoborsler@mmc.school.nz>
2025-04-29 12:21:17 +1200

    Basic string checker

---
+++ ZZ_Sandpit_v2.py
@@ -0,0 +1,16 @@
+# list exploration / experiment
+
+fruit_list = ['apple', 'banana', 'cherry', 'dragon fruit']
+
+for item in fruit_list:
+
+    print()
+
+    # print the whole word
+    print("Fruit Name: ", item)
+
+    # print the first letter
+    print("Fruit letter", item[0])
+
+    # print the first two letters
+    print(f"First two letters {item[:2]}")
